fb31539e939e5b150dd4853c0de72b71523c8e98
Michelle Brant <michelle.brant18@gmail.com>
2017-03-03 14:57:05 -0500

    new proj

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 $(document).ready(function () {
+// console.log('script loaded')
+// fontArray = [
+// "'Abril Fatface', cursive",
+// "'Slabo 27px', serif",
+// "'Oswald', sans-serif",
+// "'Montserrat', sans-serif",
+// "'Open Sans Condensed', sans-serif",
+// "'Nova Script', cursive",
+// "'Indie Flower', cursive",
+// "'Lobster', cursive",
+// "'Bungee', cursive",
+// '"Pacifico", cursive'
+// ]
+
+// function changeFonts(){
+//   bigM = $('.title_card');
+//   var randomNum = Math.floor(Math.random() * 9);
+//   font = fontArray[randomNum]
+//   bigM.css("font-family", font)
+// }
+
+// setInterval(function(){
+//   changeFonts();
+// }, 1000)
+
+
+// function changeMFonts(){
+//   var randomNum = Math.floor(Math.random() * 9);
+//   font = fontArray[randomNum]
+//   $('.name').css("font-family", font)
+// }
 
 $('.name').mouseenter(function(){
   namei = setInterval(function(){
